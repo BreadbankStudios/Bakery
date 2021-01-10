55a5289d4297ebcff4c4091f3ec15d16732dfe32
jamestkelly <jimkelly.t@outlook.com>
2021-01-10 16:47:58 +1000

    Added Logo and Authentication Scaffold
    Initial commit of first version of logo. Built framework to authenticate application using "Auth0" on a free account basis.

--- bakery-desktop-client/main.js
+++ bakery-desktop-client/main.js
@@ -1,6 +1,45 @@
 const { app, BrowserWindow } = require("electron");
-const path = require(path);
+const { createAuthWindow } = require('./main/auth-process');
+const createAppWindow = require('./main/app-process');
+const authService = require('./services/auth-service');
 
+async function showWindow() {
+    try {
+      await authService.refreshTokens();
+      return createAppWindow();
+    } catch (err) {
+      createAuthWindow();
+    }
+}
+
+// Function to create page for application home screen
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+}
+
+// Invoke when application loaded
+app.on("ready", showWindow);
+
+// Quit process if windows closed and not running on MacOS
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+// Invoke application when icon is clicked if no windows are open
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+});
+/*
 // Function to create page for application home screen
 const loadMainWindow = () => {
     const mainWindow = new BrowserWindow({
@@ -23,3 +62,10 @@ app.on("window-all-closed", () => {
         app.quit();
     }
 });
+
+// Invoke application when icon is clicked if no windows are open
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+}); */
